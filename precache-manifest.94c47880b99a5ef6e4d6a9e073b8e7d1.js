20602857be9c2daa05c4be52a706f7a2c9afd41e
Duong Tan Thanh <thanhduong12a2@gmail.com>
2019-11-07 14:40:37 +0700

    Updates

---
+++ precache-manifest.94c47880b99a5ef6e4d6a9e073b8e7d1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f81a319fb215050fa5c0c4c389e8107",
+    "url": "/redux_todo_app/index.html"
+  },
+  {
+    "revision": "09f33a4502a2cced8b26",
+    "url": "/redux_todo_app/static/css/2.f21faf4c.chunk.css"
+  },
+  {
+    "revision": "60677bfd8a2cd780e9f1",
+    "url": "/redux_todo_app/static/css/main.8f5c6ed4.chunk.css"
+  },
+  {
+    "revision": "09f33a4502a2cced8b26",
+    "url": "/redux_todo_app/static/js/2.d359fac6.chunk.js"
+  },
+  {
+    "revision": "60677bfd8a2cd780e9f1",
+    "url": "/redux_todo_app/static/js/main.b9edf206.chunk.js"
+  },
+  {
+    "revision": "0dc9bbe08165f520e497",
+    "url": "/redux_todo_app/static/js/runtime-main.2ac265f7.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/redux_todo_app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/redux_todo_app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/redux_todo_app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/redux_todo_app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/redux_todo_app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/redux_todo_app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/redux_todo_app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/redux_todo_app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/redux_todo_app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/redux_todo_app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/redux_todo_app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/redux_todo_app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/redux_todo_app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/redux_todo_app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/redux_todo_app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/redux_todo_app/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
